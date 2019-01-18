9b3be1b08890a248ed7a87719d70ca83b55cedb8
gitSnake <GitSnake@users.noreply.github.com>
2019-01-18 11:12:09 -0500

    creating new project

--- 2-jQuery/6-to-do-list/script.js
+++ 2-jQuery/6-to-do-list/script.js
@@ -2,5 +2,19 @@ $(document).ready(function(){
   $("#projects").tabs();
   $("ul").sortable({axis:"x", containment:"#projects"});
   $("ol").sortable({axis:"y", containment:"#projects"});
-  $("#btnAddProject").button();
-});
+
+  $("#btnAddProject").button()
+  .click(function(){
+    $("project-dialog").dialog(400, resizable:false, modal:true,
+      buttons:{
+        "Add new project":function(){
+          // value of the text box
+          let projectName = $("new-project").val();
+        },
+        "Cancel":function(){
+          $("#new-project").val("");
+          $(this).dialog("close");
+        }
+      });
+    });
+  });
