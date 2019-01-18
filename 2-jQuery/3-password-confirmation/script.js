155529cd5a830bec875c8ef7caf125b8bd074f6c
gitSnake <GitSnake@users.noreply.github.com>
2019-01-17 21:05:07 -0500

    Added isPassMathing function equals

--- 2-jQuery/3-password-confirmation/script.js
+++ 2-jQuery/3-password-confirmation/script.js
@@ -1,10 +1,17 @@
 $("form span").hide();
 
 $("#password").keyup();
+$("")
 
 function isPassValid() {
   return $("#password").val().length > 6;
 }
+function isPassMatching(){
+  return $("#confirm_password").val() === $("#password").val();
+}
+
+
+
 function errorMessageEvent() {
   if(isPassValid(){
     $(this).next().hide();
