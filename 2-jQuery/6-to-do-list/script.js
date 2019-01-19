0f2a711738f055784b08067e657dfa38f870ff91
gitSnake <GitSnake@users.noreply.github.com>
2019-01-19 11:24:31 -0500

    Changed places of files

---
+++ 2-jQuery/6-to-do-list/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#projects").tabs();
+  $("ul").sortable({axis:"x", containment:"#projects"});
+  $("ol").sortable({axis:"y", containment:"#projects"});
+
+  $("#btnAddProject").button()
+  .click(function(){
+    $("project-dialog").dialog(400, resizable:false, modal:true,
+      buttons:{
+        "Add new project":function(){
+          // value of the text box
+          let projectName = $("new-project").val();
+          $("<li><a href='#" + projectName + "'>" + projectName + "</a></li>")
+          .appendTo("#main");
+          $("#projects").tabs("refresh");
+          $("#new-project").val("");
+          $(this).dialog("close");
+        },
+        "Cancel":function(){
+          $("#new-project").val("");
+          $(this).dialog("close");
+        }
+      });
+    });
+  });
---
+++ 2-jQuery/jQuery-and-Ajax-learnCode/3-jQuery-tutorial/README.md
@@ -0,0 +1,1 @@
+# jQuery Navigation Bar
