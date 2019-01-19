b3f9a26a1b2b8d4d6e824da1be54695cc7cd2e9d
gitSnake <GitSnake@users.noreply.github.com>
2019-01-19 03:40:06 -0500

    Added more comments

--- 2-jQuery/3-image-slider/script.js
+++ 2-jQuery/3-image-slider/script.js
@@ -4,4 +4,7 @@ $(function(){
   //setInterval
     //animate margin-left
       //if it's last slide, go to position 1 (0px);
+
+      //listen for mouseenter and pause
+      //resume of mouseleave
 });
