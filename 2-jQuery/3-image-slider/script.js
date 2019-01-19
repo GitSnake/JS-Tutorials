468d4b5e3280dcbc801a5d8be96dfa413cd474c3
gitSnake <GitSnake@users.noreply.github.com>
2019-01-19 04:00:36 -0500

    Added currentSlide variable

--- 2-jQuery/3-image-slider/script.js
+++ 2-jQuery/3-image-slider/script.js
@@ -5,6 +5,7 @@ $(function(){
   let width = 720;
   let animationSpeed = 1000;
   let pause = 3000;
+  let currentSlide = 1;
 
   // cache DOM
   let $slider = $('#slider');
@@ -13,7 +14,9 @@ $(function(){
 
   //setInterval
   setInterval(function(){
-    $sliderContainer.animate({'margin-left': '-=' + width + 'px'}, animationSpeed);
+    $sliderContainer.animate({'margin-left': '-=' + width + 'px'}, animationSpeed, function(){
+      d
+    });
   }, pause);
     //animate margin-left
       //if it's last slide, go to position 1 (0px);
