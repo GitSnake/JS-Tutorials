70012f5efe124eb8d28a9c8dce57d80f20edff88
gitSnake <GitSnake@users.noreply.github.com>
2019-01-28 03:04:41 -0500

    updated peramiters of render method

--- 4-ReactJS/2-scrimba/1-to-do-app/index.js
+++ 4-ReactJS/2-scrimba/1-to-do-app/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-reactDom.render(WHAT DO I WANT TO REDNER, WHERE DO I WANT TO RENDER IT)
+reactDom.render(<h1>Hello World</h1>, document.getElementById("root"))
