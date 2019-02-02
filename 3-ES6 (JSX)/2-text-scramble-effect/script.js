7108ddeeb9a7ad4e8847d5580da05f3f6dc5caea
gitSnake <GitSnake@users.noreply.github.com>
2019-02-02 00:22:27 -0500

    SetText function

--- 3-ES6 (JSX)/2-text-scramble-effect/script.js
+++ 3-ES6 (JSX)/2-text-scramble-effect/script.js
@@ -10,6 +10,15 @@ class TextScramble {
   }
   setText(newText){
     const oldText = this.el.innerText;
-    const length = Math.max(oldText).length
+    const length = Math.max(oldText).length);
+    const promise = new Promise((resolve) => this.resolve = resolve);
+    this.queue = []
+    for (let i = 0; 1 < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end })
+    }
   }
 }
