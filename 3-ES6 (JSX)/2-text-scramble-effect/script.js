3778f25ff87e77dfa0d836dc4419208d999b0152
gitSnake <GitSnake@users.noreply.github.com>
2019-02-02 00:12:07 -0500

    Added class TextScramble

--- 3-ES6 (JSX)/2-text-scramble-effect/script.js
+++ 3-ES6 (JSX)/2-text-scramble-effect/script.js
@@ -1,3 +1,15 @@
 /* --------------------------------------
 * TextScramble
 -------------------------------------- */
+
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}-=+*^?#________';
+    this.update = this.update.bind(this);
+  }
+  setText(newText){
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText).length
+  }
+}
