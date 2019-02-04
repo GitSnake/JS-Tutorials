a05f25986791560da09f3b61afbef98f57f4e922
gitSnake <GitSnake@users.noreply.github.com>
2019-02-04 18:30:04 -0500

    moved drawing code into drawEverthing

--- 3-ES6 (JSX)/2-arcade-classic-game/script.js
+++ 3-ES6 (JSX)/2-arcade-classic-game/script.js
@@ -5,6 +5,9 @@ window.onload = function() {
   console.log("Hello World!");
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext('2d');
+}
+
+const drawEverything => () {
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   canvasContext.fillStyle = 'red';
@@ -12,5 +15,3 @@ window.onload = function() {
   canvasContext.fillStyle = 'white';
   canvasContext.fillRect(225,210,200,200);
 }
-
-const drawEverything => () {}
