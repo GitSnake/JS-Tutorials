148a0d4186907b584eeeb3c652fad517d0d81235
gitSnake <GitSnake@users.noreply.github.com>
2019-02-04 16:18:17 -0500

    Update numbers-challenge.js

---
+++ 1-Javascript (Raw)/1-scrimba-intro-javascript/2-numbers-challenge/numbers-challenge.js
@@ -0,0 +1,9 @@
+let example1 = parseInt("Hello 33 World 22");
+let example2 = parseFloat('44 Dylan 33');
+let example3 = 55.3333.toFixed(0);
+let example4 = 200.0.toFixed(2);
+
+console.log();
+console.log();
+console.log();
+console.log();
