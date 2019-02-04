812dbe2f20392234e99519d7667d47ccc1f8ad52
gitSnake <GitSnake@users.noreply.github.com>
2019-02-04 17:08:31 -0500

    Added canvasContext

--- 3-ES6 (JSX)/2-arcade-classic-game/script.js
+++ 3-ES6 (JSX)/2-arcade-classic-game/script.js
@@ -4,5 +4,7 @@ let canvasContext;
 window.onload = function() {
   console.log("Hello World!");
   canvas = document.getElementById("gameCanvas");
-
+  canvasContext = canvas.getContext('2d');
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0,0,canvas.width,canvas.height;
 }
