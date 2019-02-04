26de6af6428915c0e58ee53fdf80be4dbbc78eab
gitSnake <GitSnake@users.noreply.github.com>
2019-02-04 18:20:52 -0500

    Drawing and Positioning 1

--- 3-ES6 (JSX)/2-arcade-classic-game/script.js
+++ 3-ES6 (JSX)/2-arcade-classic-game/script.js
@@ -6,5 +6,9 @@ window.onload = function() {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext('2d');
   canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0,0,canvas.width,canvas.height;
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(200,200,50,25);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(225,210,200,200);
 }
