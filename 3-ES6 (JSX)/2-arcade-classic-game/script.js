5244e6719e7e31d1eafb7d5b099d656f54ce5054
gitSnake <GitSnake@users.noreply.github.com>
2019-02-14 19:38:31 -0500

    Fixing code

--- 3-ES6 (JSX)/2-arcade-classic-game/script.js
+++ 3-ES6 (JSX)/2-arcade-classic-game/script.js
@@ -1,3 +1,4 @@
+
 // canvas variables
 const canvas;
 let canvasContext;
@@ -8,6 +9,7 @@ const ballSpeedX = 5;
 const ballY = 50;
 const ballSpeedY  = 4;
 // paddle variables
+
 const paddle1Y = 250;
 const paddle2Y = paddle1Y;
 const paddleThickness = 10;
@@ -15,17 +17,6 @@ const paddleHeight = 100;
 // score display variables
 let player1Score = 0;
 let player2Score = 0;
-
-const calculateMousePos => (evt){
-  let rect = canvas.getBoundingClientRect();
-  let root = document.documentElement;
-  let mouseX = evt.clientX - rect.left - root.scrollLeft;
-  let mouseY = evt.clientY - rect.top - root.scrollTop;
-  return {
-    x:mouse,
-    y:mouseY
-  };
-}
 
 
 window.onload => () {
@@ -40,12 +31,90 @@ window.onload => () {
       let mousePos = calculateMousePos(evt);
       paddle1Y = mousePos.y-(paddleHeight/2);
     });
+
+
 }
 
 const ballReset => (){
   ballSpeedX = -ballSpeedX;
   ballX = canvas.width/2;
   ballX = canvas.height/2;
+}
+
+  const moveEverything => () {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+// ball direction
+    if (ballX < 0 ) {
+      if (ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
+        ballSpeedX = - ballX;
+      }else {
+        ballReset();
+      }
+    }
+    if (ballX > canvas.width) {
+      ballSpeedX = -ballSpeedX;
+    }
+    if (ballY < 0 ) {
+      ballSpeedY = -ballSpeedY;
+    }
+    if (ballY > canvas.width) {
+      ballSpeedY = -ballSpeedX;
+    }
+
+  }
+
+  const drawEverything => () {
+    // creates black screen
+    colorRect.fillRect(0,0,canvas.width,canvas.height,'black');
+    // creates left player paddle
+    colorRect.fillRect(0,paddle1Y,paddleThickness,paddleHeight,'white');
+    // creates right computer paddle
+    colorRect.fillRect(canvas.width - paddleThickness, paddle2Y, paddleThickness, paddleHeight,'white');
+    // creates ball
+    colorCircle(ballX, ballY, 10, 'white');
+    // score display
+    canvasContext.fillText(player1Score, 100, 100);
+    canvasContext.fillText(player2Score,canvas.width - 100, 100);
+  }
+
+  const colorCircle => (centerX, centerY, radius, drawColor){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
+  }
+  const colorRect => (leftX, topY, width, height, drawColor){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+  }
+
+
+const calculateMousePos => (evt){
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+  let mouseX = evt.clientX - rect.left - root.scrollLeft;
+  let mouseY = evt.clientY - rect.top - root.scrollTop;
+  return {
+    x:mouse,
+    y:mouseY
+  };
+}
+
+/*
+
+window.onload => () {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext('2d');
+
+  let framesPerSecond = 30;
+  setInterval( const callBoth => (){ moveEverything(); drawEverything() }, 1000/framesPerSecond );
+
+  canvas.addEventListener('mousemove',
+    (function(evt) {
+      let mousePos = calculateMousePos(evt);
+      paddle1Y = mousePos.y-(paddleHeight/2);
+    });
 }
 
 const computerMovement => (){
@@ -62,7 +131,7 @@ const moveEverything => () {
 
 computerMovement();
 
-  ballX += ballSpeedX;
+
 
   if (ballX < 0) || (ballX > canvas.width) {
     if (ballY > paddle1Y && ballY < paddle1Y+paddleHeight) {
@@ -87,27 +156,4 @@ computerMovement();
   }
 }
 
-const drawEverything => () {
-  // creates black cover
-  colorRect.fillRect(0,0,canvas.width,canvas.height,'black');
-  // creates left paddle
-  colorRect.fillRect(0,paddle1Y,paddleThickness,paddleHeight,'white');
-  // creates right paddle
-  colorRect.fillRect(canvas.width - paddleThickness, paddle2Y, paddleThickness, paddleHeight,'white');
-  // creates ball
-  colorCircle(ballX, ballY, 10, 'white');
-  // score display
-  canvasContext.fillText(player1Score, 100, 100);
-  canvasContext.fillText(player2Score,canvas.width - 100, 100);
-}
-
-const colorCircle => (centerX, centerY, radius, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.beginPath();
-  canvasContext.arc(centerX, centerY, radius, 0, Math.Pi*2, true);
-  canvasContext.fill();
-}
-const colorRect => (leftX, topY, width, height, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect(leftX, topY, width, height);
-}
+*/
