c94025a17c7a899c4077dcd17f8b8b821f0f2158
gitSnake <GitSnake@users.noreply.github.com>
2019-04-14 17:09:36 -0400

    create audio files

--- 3-ES6 (JSX)/2-js-games/1-snake-game/script.js
+++ 3-ES6 (JSX)/2-js-games/1-snake-game/script.js
@@ -1,5 +1,11 @@
 const csv = document.getElementById('canvas');
 const ctx = csv.getContext('2d');
+
 // Load images
 let imageName = new Image();
 imageName.src = "path/image.png";
+
+// Load audio
+let audioName = new Audio();
+audioName.src = "path/audio.png";
+audioName.play();
