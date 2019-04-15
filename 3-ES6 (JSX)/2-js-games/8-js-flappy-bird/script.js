ef955157c32c9907ed92bc9b5d7f42cc6845be78
gitSnake <GitSnake@users.noreply.github.com>
2019-04-14 23:30:17 -0400

    Adding file variables

---
+++ 3-ES6 (JSX)/2-js-games/8-js-flappy-bird/script.js
@@ -0,0 +1,8 @@
+// variables
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+
+let imageName = new Image();
+imageName.src = "images/img.png";
+let audioName = new Audio;
+audioName.src = "audio/audio.png";
