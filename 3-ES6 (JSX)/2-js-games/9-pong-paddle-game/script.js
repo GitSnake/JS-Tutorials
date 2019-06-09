ff15bde70621fd7fcecef065b13a4a575f7aad1e
gitSnake <GitSnake@users.noreply.github.com>
2019-06-09 01:01:27 -0400

    Added onload function with basic drawing function

---
+++ 3-ES6 (JSX)/2-js-games/9-pong-paddle-game/script.js
@@ -0,0 +1,6 @@
+window.onload = function{
+canvas = document.getElementById('gameCanvas');
+canvasContext = canvas.getContext('2d');
+canvasContext.fillStyle = 'black';
+canvasContext.fillRect(0,0,canvas.width,canvas.height);
+}
